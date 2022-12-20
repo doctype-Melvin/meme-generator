a17a86918bf50856116212fe6f4c4c66545158ef
doctype-Melvin <doctype.melvin@gmail.com>
2022-12-20 13:27:34 +0100

    working MVP meme generator app

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,6 +3,11 @@ import { useState } from "react";
 import data from '../../data'
 
 export default function Form() {
+
+    const [formData, setFormData] = useState(
+        {topText: '', bottomText: ''}
+        )
+
     const [meme, setMeme] = useState({
         topText: '',
         bottomText: '',
@@ -10,6 +15,19 @@ export default function Form() {
     })
 
     const [allMemeImages, setMemeImg] = useState(data)
+
+    console.log(formData)
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+        setFormData(prevState => {
+            return {
+                ...prevState,
+                [name]: value
+            }
+        }
+        )
+    }
 
     const handleClick = (e) => {
         e.preventDefault()
@@ -24,20 +42,31 @@ export default function Form() {
         
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(formData)
+    }
+
     return (
         <main className="formContainer">
             <div className="mainContainer">
-                <form>
-                    <div className="inputs">
+                <form onSubmit={handleSubmit}>
+                    <div className="inputs" >
                         <input
+                        onChange={handleChange}
+                        name='topText'
                         className="topText"
                         type='text'
                         placeholder="Top Text"
+                        value={formData.topText}
                         ></input>
                         <input
+                        onChange={handleChange}
+                        name="bottomText"
                         className="bottomText"
                         type='text'
                         placeholder="Bottom Text"
+                        value={formData.bottomText}
                         ></input>
                     </div>
                     <button 
@@ -46,7 +75,11 @@ export default function Form() {
                         Get meme
                     </button>
                 </form>
-                <img className="memeImg" src={meme.randomImg} />
+                <div className="meme">
+                    <img className="memeImg" src={meme.randomImg} />
+                    <h2 className="memeText top">{formData.topText}</h2>
+                    <h2 className="memeText bottom">{formData.bottomText} </h2>
+                </div>
             </div>
             
         </main>
